a9078d45078f2f0e9f50a10b5a47d4ae9016cf1a
Benjamin Feight <bjfeight@gmail.com>
2016-09-12 10:33:27 -0400

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,4 +64,8 @@ require 'test_helper'
         assert_not @user.valid?
         end
 
+      test "authenticated? should return false for a user with nil digest" do
+        assert_not @user.authenticated?('')
+      end
+
 end
